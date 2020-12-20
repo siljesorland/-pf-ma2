1b49706134143baae42b979effd538353ae74438
Silje Sørland <72661374+siljesorland@users.noreply.github.com>
2020-12-20 11:54:17 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+Question 1
+var petArray = ["cat", "cow", "dog"]
+
+Qestion 2
+console.log(petArray[1]);
+
+Question 3
+petArray.push("sheep");
+
+Question 4
+console.log(petArray.length);
+
+Question 5
+var catObject = {
+name: "Missy",
+color: "black",
+age:  "4"
+};
+
+Question 6
+console.log(catObject[2]); 
+
+Question 7
+var catArray = [{
+name: "Missy",
+color: "black",
+age:  "4"
+};
+]
+
+Question 8
+for (var i = 0; i<catArray; i ++) {
+ Console.log(catArray[i]);
+}
+
+Question 9
+function logToConsole() {
+	console.log("name");
+}
+
+Question 10
+logToConsole("donkey");
